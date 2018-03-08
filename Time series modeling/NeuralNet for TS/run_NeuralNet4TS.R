50cfebe2d4c2198747f20e1ad1bbd81ad4b6ec2e
awash-analytics <awash.analytics@gmail.com>
2018-03-08 08:59:17 +0100

    forecast package lab
    forecast package lab

--- Time series modeling/NeuralNet for TS/run_NeuralNet4TS.R
+++ Time series modeling/NeuralNet for TS/run_NeuralNet4TS.R
@@ -8,7 +8,7 @@
 source("./load library.R")
 
 ## load lag calculator
-source("./lag_calculator.R")
+# source("./lag_calculator.R")
 
 
 ## --------------------------------------------------- ##
@@ -38,50 +38,70 @@ plot(y2, xlab='Date', ylab='Number of Cases', col='darkblue')
 pacf(y2)
 
 ## derive lag variables
-data_unstd <- lag_calculator(y = y, n_lags = 4)
+# data_unstd <- lag_calculator(y = y, n_lags = 4)
 # data_std <- lag_calculator(y = y, n_lags = 4, scale = TRUE, scale_type = "range")
 
 
 ## -- Step 3. Training a Model on the Data
 # data <- data_unstd
-data <- data_unstd 
+# data <- data_unstd
+# data <- y2
 
 ## partition dataset for training/testing
-set.seed(pi)
-
-myTimeControl <- trainControl(method = "timeslice", 
-                              initialWindow = 40, 
-                              horizon = 12, 
-                              fixedWindow = TRUE)
-
-# m1 <- caret::train(y ~ ., data = data, method = "nnet", linout=TRUE, trace=FALSE)
-m2 <- caret::train(y ~ ., data = data, 
-                   method = "neuralnet", 
-                   preProc = c("center", "scale"), 
-                   hidden = c(10, 2),
-                   trControl = myTimeControl
-                   # ,  
-                   # algorithm = 'backprop', learningrate = 0.25, hidden = 3, linout = TRUE
-                   )
+# set.seed(pi)
+# 
+# myTimeControl <- trainControl(method = "timeslice", 
+#                               initialWindow = 40, 
+#                               horizon = 12, 
+#                               fixedWindow = TRUE)
+# 
+# # m1 <- caret::train(y ~ ., data = data, method = "nnet", linout=TRUE, trace=FALSE)
+# m2 <- caret::train(y ~ ., data = data, 
+#                    method = "neuralnet", 
+#                    preProc = c("center", "scale"), 
+#                    hidden = c(10, 2),
+#                    trControl = myTimeControl
+#                    # ,  
+#                    # algorithm = 'backprop', learningrate = 0.25, hidden = 3, linout = TRUE
+#                    )
 
 ## using pkg::forecast
-m3 <- forecast::nnetar(y = y, lambda = 0.5)
-m3
+fit_nnetar_default <- forecast::nnetar(y = y2)
+fit_nnetar_default
 
-sim <- ts(matrix(0, nrow=20, ncol=9), start=end(y)[1]+1)
-for(i in seq(9))
-  sim[,i] <- simulate(m3, nsim=20)
+fcast_default <- forecast::forecast(object = fit_nnetar_default, PI = TRUE)
+plot(fcast_default)
+autoplot(fcast_default)
 
-library(ggplot2)
-autoplot(y) + forecast::autolayer(sim)
+fit_nnetar <- forecast::nnetar(y = y2, p = 4, size = 4)
+fit_nnetar
+# plot(fit_nnetar)     ## failed
 
-## prediction
-pred_m1 <- predict(m1, data)
+fcast <- forecast::forecast(object = fit_nnetar, PI = TRUE)
+plot(fcast)
+autoplot(fcast)
 
-## Examine results
-pred_m1_ts <- ts(pred_m1, start = c(2001, 1), end = c(2013, 20), frequency = 52)
+fcast_summary <- summary(fcast)
+mean <- fcast$mean
 
-plot(y2, xlab='Date', ylab='Number of Cases', col='2')
-lines(pred_m1_ts, col=3)
-# legend(5, 70, c("y", "pred"), cex=1.5, fill=2:3)
+
+fcast_fitvalues <- fitted.values(fcast)
+# plot(fcast_summary)
+
+# sim <- ts(matrix(0, nrow=20, ncol=9), start=end(y)[1]+1)
+# for(i in seq(9))
+#   sim[,i] <- simulate(m3, nsim=20)
+# 
+# library(ggplot2)
+# autoplot(y) + forecast::autolayer(sim)
+# 
+# ## prediction
+# pred_m1 <- predict(m1, data)
+# 
+# ## Examine results
+# pred_m1_ts <- ts(pred_m1, start = c(2001, 1), end = c(2013, 20), frequency = 52)
+# 
+# plot(y2, xlab='Date', ylab='Number of Cases', col='2')
+# lines(pred_m1_ts, col=3)
+# # legend(5, 70, c("y", "pred"), cex=1.5, fill=2:3)
 
